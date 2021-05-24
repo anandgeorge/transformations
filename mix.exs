aa7b9a33c7e11adf5f66b8b08fa99625d3279e7b
Anand George <mranandgeorge@gmail.com>
2021-05-24 16:42:48 +0530

    Publish hex package transformations

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule Transformations.MixProject do
       app: :transformations,
       version: "0.1.0",
       elixir: "~> 1.10.2",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "Elixir library for transforming 3D homogeneous coordinates",
+      deps: deps,
+      package: package,
+      name: "Transformations",
+      source_url: "https://github.com/anandgeorge/transformations"
     ]
   end
 
@@ -25,4 +30,21 @@ defmodule Transformations.MixProject do
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
     ]
   end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "README.md",
+        "mix.exs"
+      ],
+      maintainers: [
+        "Anand George"
+      ],
+      licenses: ["simplified BSD"],
+      links: %{
+        "GitHub" => "https://github.com/anandgeorge/transformations"
+      }
+    ]
+  end  
 end
